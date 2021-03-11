be7c712951dad2b5c14317915646788d6cd19bed
Anton Borkovskij <member@tut.by>
2021-03-11 21:07:47 +0300

    fix: cards styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,13 @@ export default function Card(props) {
 
     return (
         <li className='card' id={id}>
-            <span className='title'>{title}</span>
-            <div className='desc'>{desc}</div>
-            <span className='user'>{user}</span>
+            <span className='card--title'>{title}</span>
+            <div className='card--desc'>{desc}</div>
+            <span className='card--user'>{user}</span>
             <div className='card--bottom'>
-                <span className='date'>{date}</span>
-                <button className='delete-btn' />
-                <button className='move-btn' />
+                <span className='card--date'>{date}</span>
+                <button className='card--delete-btn' />
+                <button className='card--move-btn' />
             </div>
         </li>
     )
--- src/components/Column.js
+++ src/components/Column.js
@@ -1,4 +1,6 @@
 import { useContext, useEffect } from "react";
+import uuid from 'react-uuid';
+
 import AddCardModal from "./AddCardModal";
 import Card from './Card'
 import {CardsContext} from '../App';
@@ -20,8 +22,10 @@ export default function Column({name}) {
                 <div className="board--delBtn"></div>
             </div>
             <ul className="board__cards">
-                {cards.length ?
-                    name === 'To do' && cards.filter(el=>el.column===0).map(item=> <Card data={item} /> ) 
+                {cards.length ? 
+                    (name==='To do' && cards.filter(el=>el.column===0).map(item=> <Card key={uuid()} data={item} />)) ||
+                    (name==='In Progress' && cards.filter(el=>el.column===1).map(item=> <Card key={uuid()} data={item} /> )) ||
+                    (name==='Done' && cards.filter(el=>el.column===2).map(item=> <Card key={uuid()} data={item} /> ))  
                     : <p>no cards</p>}
             </ul>
             {name === 'To do' && <AddCardModal/>}
